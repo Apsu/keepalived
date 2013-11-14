3a7644c70b4985dc13781898fc7020e51b7301b9
root <root@j-a-t-1-chef.(none)>
2013-11-14 22:28:57 +0000

    Adjust behavior to delegate to local route table on ingress only

--- files/default/notify.sh
+++ files/default/notify.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
 
+# Args from keepalived notify_*
 action=$1
 iface=$2
 vip=$3
 src=$4
 
+# Custom routing table name/index
+table=vips
+index=100
+
+# Ensure persistent table config
+if ! grep -q "$index $table" /etc/iproute2/rt_tables; then echo "$index $table" >> /etc/iproute2/rt_tables; fi
+
+# Setup our custom table and rule
+ip route add local $vip dev $iface table vips
+if ! (ip rule list | grep -q $table); then ip rule add iif $iface lookup $table; fi
+
 # Make sure forwarding is on
 sysctl net.ipv4.ip_forward=1
 
 case $action in
-  add)
+  add|haproxy)
     logger -t keepalived-notify-$action "Removing local route for $vip"
     ip route del table local local $vip
-
+    ;;& # Check remaining patterns
+  add)
     logger -t keepalived-notify-$action "Adding VIP NATs for $vip"
     while ! iptables -t nat -I PREROUTING -d $vip/32 -j DNAT --to-dest $src; do sleep 1; done
     while ! iptables -t nat -I OUTPUT -d $vip/32 -j DNAT --to-dest $src; do sleep 1; done
-    while ! iptables -t nat -I POSTROUTING -m conntrack --ctstate DNAT --ctorigdst $vip/32 -j SNAT --to-source $vip; do sleep 1; done
     ;;
   del)
     logger -t keepalived-notify-$action "Deleting VIP NATs for $vip"
     iptables -t nat -D PREROUTING -d $vip/32 -j DNAT --to-dest $src
     iptables -t nat -D OUTPUT -d $vip/32 -j DNAT --to-dest $src
-    iptables -t nat -D POSTROUTING -m conntrack --ctstate DNAT --ctorigdst $vip/32 -j SNAT --to-source $vip
     ;;
 esac
